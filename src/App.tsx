a26857d2ac37b9755094ae1b7979b823f318b18a
Awsaf Fida Mahmud <fidaawsaf@gmail.com>
2025-02-05 12:28:10 -0500

    Vite template and simple file routing

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import {Link, Route, Routes} from "react-router-dom";
+import {Login} from "./Pages/Login.tsx";
+import {Register} from "./Pages/Register.tsx";
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <nav>
+          <Link to={'/'}>Home</Link>
+          <Link to={'/login'}>login</Link>
+          <Link to={'/register'}>register</Link>
+
+      </nav>
+
+        <Routes>
+            <Route path={"/login"} element={<Login/>}/>
+            <Route path={"/register"} element={<Register/>}/>
+        </Routes>
+    </>
+  )
+}
+
+export default App
